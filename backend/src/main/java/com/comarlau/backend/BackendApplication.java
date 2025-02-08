7433e1be9cdab8b397e8988978d66b99e42b9b77
vladcomarlau <vladcom95@gmail.com>
2025-02-08 21:41:48 +0200

    modify dockerfiles for use with aws

--- backend/src/main/java/com/comarlau/backend/BackendApplication.java
+++ backend/src/main/java/com/comarlau/backend/BackendApplication.java
@@ -2,13 +2,27 @@ package com.comarlau.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@RequestMapping("/")
+@RestController
 public class BackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
-        System.out.println("Hello World!");
+        System.out.println("Hello Vlad!");
     }
 
+    @GetMapping("/")
+    public String hello() {
+        return "Hello home!";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello alternative page!";
+    }
 }
