92d5a4d44b85a5dc32fed235275b4d9d0c1c920d
vladcomarlau <102293760+vladcomarlau@users.noreply.github.com>
2025-03-25 01:28:19 +0200

    Webhook test

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
     return(
         <>
-            Business Management Home
+            Business Management Home webhook test
         </>
     )
 }
