5b32bd726392a7f912f63db8009e445366f3b007
vladcomarlau <vladcom95@gmail.com>
2025-03-26 00:50:25 +0200

    - update frontend home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 export default function Home() {
+    const [setVal, val] = React.useState('');
+    const apiUrl = `http://business-backend-svc:8080`;
+    fetch(`${apiUrl}/helloworld`)
+        .then(response => response.json())
+        .then(data => setVal(data));
+
     return(
         <>
             Business Management Home webhook test
+            Backend response: {val}
         </>
     )
 }
