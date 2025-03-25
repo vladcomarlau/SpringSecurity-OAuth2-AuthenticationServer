a1419ab225cf68a64c98288bc8110c9f94ad613f
vladcomarlau <vladcom95@gmail.com>
2025-03-25 21:54:56 +0200

    - add project packages (basic structure) - add sample REST controller

---
+++ backend/src/main/java/com/comarlau/backend/controller/basicController.java
@@ -0,0 +1,15 @@
+package com.comarlau.backend.controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class basicController {
+
+    @GetMapping("/helloworld")
+    public String hello() {
+        return"Hello World!";
+    }
+}
