61945868e317541366ff2128aa8d6dab02a90530
vladcomarlau <vladcom95@gmail.com>
2025-04-02 16:04:31 +0300

    - move CORS to config class

---
+++ backend/src/main/java/com/comarlau/backend/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.comarlau.backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    // This property can contain one or more origins separated by commas
+    @Value("${app.allowed.origins}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
+    }
+}
--- backend/src/main/java/com/comarlau/backend/controller/basicController.java
+++ backend/src/main/java/com/comarlau/backend/controller/basicController.java
@@ -1,13 +1,11 @@
 package com.comarlau.backend.controller;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@CrossOrigin(origins = "https://comarlau.com/business-management/api", allowCredentials = "true")
 public class basicController {
     @GetMapping("/helloworld")
     public String hello() {
