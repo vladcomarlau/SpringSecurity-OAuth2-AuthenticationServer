961829b967e0cdeea4438f192101209c075126be
vladcomarlau <vladcom95@gmail.com>
2025-04-13 15:18:53 +0300

    change create-react-app to VITE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import Info from "./pages/Info";
 
 function App() {
   return (
-    <>
-      <BrowserRouter basename={process.env.REACT_APP_ROOT_URL}>
+    <>test
+      <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/info" element={<Info/>} />
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { useState } from "react";
+import {useState} from 'react';
 import axios from 'axios';
 
 export default function Home() {
     const [val, setVal] = useState("");
 
-    axios.get(process.env.REACT_APP_API_URL + "/helloworld", {
+    axios.get(import.meta.env.VITE_API_URL + "/helloworld", {
     }).then((response) => {
         setVal(response.data);
     }).catch(function (error) {
