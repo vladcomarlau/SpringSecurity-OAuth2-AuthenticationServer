9111d0372d32b3646b2ec7c08e6221c3b5dd14b3
vladcomarlau <vladcom95@gmail.com>
2025-04-13 17:29:59 +0300

    - frontend: update base_url

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,17 @@ import Home from "./pages/Home";
 import Info from "./pages/Info";
 
 function App() {
-  return (
-    <>
-      <BrowserRouter basename={import.meta.env.VITE_BASE_URL}>
-        <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/info" element={<Info/>} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    console.log(import.meta.env.VITE_BASE_URL);
+    return (
+        <>Test
+            <BrowserRouter basename={import.meta.env.VITE_BASE_URL}>
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/info" element={<Info/>} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(()=> {
-    const basePath = process.env.VITE_BASE_URL;
+export default defineConfig(({ mode })=> {
+    const env = loadEnv(mode, process.cwd());
+    console.log('Current Mode:', mode);
+    console.log('VITE_BASE_URL:', env.VITE_BASE_URL);
     return {
-        base: '/business-management/',
+        base: env.VITE_BASE_URL,
         plugins: [
             react(),
             tailwindcss(),
