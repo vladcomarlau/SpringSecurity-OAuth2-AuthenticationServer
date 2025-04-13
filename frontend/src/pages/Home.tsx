28515209535ce50b6ec1f4d5c46408cb99363685
vladcomarlau <vladcom95@gmail.com>
2025-04-13 18:51:43 +0300

    - [frontend] update home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 export default function Home() {
     const [val, setVal] = useState("");
 
-    axios.get(import.meta.env.VITE_API_URL + "/helloworld", {
+    axios.get(import.meta.env.VITE_API_URL + "helloworld", {
     }).then((response) => {
         setVal(response.data);
     }).catch(function (error) {
